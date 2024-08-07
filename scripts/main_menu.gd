1c9ce2b704dbddc42160d2ce4dcf3ec1f50a311a
alegottu <alexgottuso@gmail.com>
2024-08-07 01:04:48 -0700

    added simple menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,36 @@
+extends Node
+
+@export var main_scene: PackedScene
+@export var main_buttons: Node
+@export var settings_menu: Node
+
+func _on_play_button_down():
+	print(main_scene)
+	get_tree().change_scene_to_packed(main_scene)
+
+func _on_quit_button_down():
+	get_tree().quit()
+
+func _on_settings_button_down():
+	settings_menu.visible = true
+	main_buttons.visible = false
+
+func _on_back_button_down():
+	settings_menu.visible = false
+	main_buttons.visible = true
+
+func _on_window_mode_item_selected(index):
+	var window: Window = get_window()
+	
+	match index:
+		0:
+			window.mode = Window.MODE_WINDOWED
+			window.borderless = false
+		1:
+			window.mode = Window.MODE_EXCLUSIVE_FULLSCREEN
+		2:
+			window.borderless = true
+			window.mode = Window.MODE_MAXIMIZED
+		3:
+			window.borderless = true
+			window.mode = Window.MODE_WINDOWED
